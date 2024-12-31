7ce5d5211565924deddb432641900fefacfba6af
Usama Khan <usama608474@gmail.com>
2024-12-31 11:40:28 +0500

    files

---
+++ app.js
@@ -0,0 +1,119 @@
+// QUESTION 02 
+
+document.write("<p>Question 02</p>");
+
+// let num1 = +prompt("Enter First Number");
+// let num2 = +prompt("Enter Second Number");
+
+// if (num1 > num2) {
+//   alert(num1 + " is the Greater Number")
+// }
+// else if (num1 == num2) {
+//   alert("Both Numbers are Equal")
+// }
+// else {
+//   alert(num2 + " is the Greater Number")
+// }
+
+
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+
+
+// QUESTION 03 
+
+document.write("<p>Question 03</p>");
+
+// let num1 = +prompt("Enter Number");
+
+// if (num1 > 0) {
+//   alert("Number is Positive")
+// }
+// else if (num1 == 0) {
+//   alert("Number is Zero")
+// }
+// else {
+//   alert("Number is Negative")
+// }
+
+
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+
+
+
+// QUESTION 04 
+
+document.write("<p>Question 04</p>");
+
+// let text = prompt("Enter The Alphabet");
+
+// if (text == "a" || text == "e" || text == "i" || text == "o" || text == "u") {
+//   alert("Character is Vowel")
+// } else {
+//   alert("Character is Consonant")
+// }
+
+
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+
+
+
+// QUESTION 05
+
+document.write("<p>Question 05</p>");
+
+// let pass = "usamakhan"
+// let pass1 = prompt("Enter Your Password")
+
+// if (pass == pass1) {
+//   alert("Correct Password")
+// }
+// else if (pass1 == "" ) {
+//   alert("Please Enter Your Password")
+// }
+// else {
+//   alert("Incorrect Password")
+// }
+
+
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+
+
+
+// QUESTION 07
+
+document.write("<p>Question 07</p>");
+
+// let time = +prompt("Enter time")
+
+// if (time >= "0000" & time < "1200") {
+//   alert("Good Morning")
+// }
+// else if (time >= "1200" & time < "1700") {
+//   alert("Good Afternoon")
+// }
+// else if (time >= "1700" & time < "2100") {
+//   alert("Good Evening")
+// }
+// else {
+//   alert("Good Night")
+// }
